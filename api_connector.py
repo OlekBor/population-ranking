739c362bc2da82f691340f14b984d6fce5edebb3
OlekBor <alexbor99@gmail.com>
2022-12-07 11:48:38 +0100

    renamed "api-connector.py" to "api_connector.py"

---
+++ api_connector.py
@@ -0,0 +1,15 @@
+import requests
+
+class APIConnector():
+
+    def __init__(self, base_uri: str):
+        self.base_uri = base_uri
+
+    def set_base_uri(uri: str):
+        self.base_uri = uri
+
+    def get(self, endpoint: str):
+        return requests.get(self.base_uri + endpoint)
+
+if __name__ == "__main__":
+    pass
