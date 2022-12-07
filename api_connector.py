40fdcec894da5fed716d72a0553a29186bd18f80
OlekBor <alexbor99@gmail.com>
2022-12-07 11:57:50 +0100

    Added RandomDataAPI class which GETs random addresses from API

--- api_connector.py
+++ api_connector.py
@@ -11,5 +11,15 @@ class APIConnector():
     def get(self, endpoint: str):
         return requests.get(self.base_uri + endpoint)
 
+class RandomDataAPI(APIConnector):
+
+    def __init__(self, base_uri='https://random-data-api.com/api/v2/'):
+        super().__init__(base_uri)
+
+    def new_random_address(self):
+        request_result = self.get("addresses")
+        return request_result.json()
+
 if __name__ == "__main__":
-    pass
+    address = RandomDataAPI().new_random_address()
+    print(address['country'])
