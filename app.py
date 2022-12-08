4c3082d9424a6736045aa755c4f2621a204c0abf
OlekBor <alexbor99@gmail.com>
2022-12-08 13:09:48 +0100

    extracted input into separate function now checking user input with limits

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ def n_random_countries(quantity: int):
         countries.append(country)
     return countries
 
+def user_input(lim: tuple[int, int]):
+    lim_str = "[{} {})".format(*lim)
+    quantity = int(input(f"How much random countries would you like to generate {lim_str}: "))
+    while quantity not in range(*lim):
+        quantity = int(input(f"Wrong input, try again in range {lim_str} [or Ctrl+D to exit]: "))
+    return quantity
+
 def main():
-    quantity = int(input("How much random countries would you like to generate: "))
+    quantity = user_input((5, 21))
     countries = n_random_countries(quantity)
     print(countries)
 
