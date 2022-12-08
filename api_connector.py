e318eb8fb3dfb9a64135df57557103d396022fec
OlekBor <alexbor99@gmail.com>
2022-12-08 12:34:57 +0100

    Added RestCountries connector to request country information

--- api_connector.py
+++ api_connector.py
@@ -20,6 +20,17 @@ class RandomDataAPI(APIConnector):
         request_result = self.get("addresses")
         return request_result.json()
 
+class CountriesAPI(APIConnector):
+
+    def __init__(self, base_uri='https://restcountries.com/v3.1/'):
+        super().__init__(base_uri)
+
+    def name_search(self, name: str):
+        return self.get(f"name/{name}").json()
+
 if __name__ == "__main__":
-    address = RandomDataAPI().new_random_address()
-    print(address['country'])
+    api_response = CountriesAPI().name_search("asdf")
+    try:
+        print(api_response[0]["population"])
+    except KeyError:
+        print("No information found")
